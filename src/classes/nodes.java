36bc0e828f050809afd2e1312d615af3ced1a1db
Harry Guthrie <GTGuthers@gmail.com>
2017-08-25 21:09:38 +1000

    Inital Classes

---
+++ src/classes/nodes.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Node {
+    public Location location;
+
+    public Node (int x, int y) {
+        this.location = new Location(x,y);
+
+    }
+
+
+}
---
+++ src/classes/tribe.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class Tribe {
+
+    public Tribe(int x, int y) {
+
+    }
+
+}
