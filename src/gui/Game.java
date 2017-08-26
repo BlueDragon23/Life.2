c1fcdecae2b1f3e59ebbf2937bad58332947f28a
BlueDragon23 <aidan_goldthorpe@outlook.com>
2017-08-26 20:44:34 +1000

    Implemented scrolling properly, that was too fucking dumb.

--- src/gui/Game.java
+++ src/gui/Game.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -19,13 +20,15 @@ public class Game {
     private Canvas canvas;
 
     @FXML
-    private Pane pane;
+    public ScrollPane scroll;
 
     private Location bottomLeft = new Location(-50, -50);
     private Location topRight = new Location(50, 50);
     private int viewSize = 10;
 
     public void drawMap(Map m) {
+        canvas.setWidth((m.getMaxX() - m.getMinX()) * viewSize);
+        canvas.setHeight((m.getMaxY() - m.getMinY()) * viewSize);
         GraphicsContext g = canvas.getGraphicsContext2D();
         g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         // Make sure the bottomLeft is the bottomLeft of the screen
@@ -58,18 +61,6 @@ public class Game {
         }
     }
 
-    public void setWidthChangeListener(ReadOnlyDoubleProperty prop) {
-        prop.addListener(new WidthChangeListener());
-    }
-
-    public void setHeightChangeListener(ReadOnlyDoubleProperty prop) {
-        prop.addListener(new HeightChangeListener());
-    }
-
-    public Pane getRootPane() {
-        return pane;
-    }
-
     private class WidthChangeListener implements ChangeListener<Number> {
 
         @Override
@@ -96,6 +87,19 @@ public class Game {
         if (viewSize > 5) {
             viewSize -= 5;
         }
+    }
+
+    // https://stackoverflow.com/a/35331595
+    public void showBounds(ScrollPane scrollPane) {
+
+        double hValue = scrollPane.getHvalue();
+        double vValue = scrollPane.getVvalue();
+        double width = scrollPane.viewportBoundsProperty().get().getWidth();
+        double height = scrollPane.viewportBoundsProperty().get().getHeight();
+
+        double x = (scrollPane.getContent().getBoundsInParent().getWidth() - width) * hValue;
+        double y = (scrollPane.getContent().getBoundsInParent().getHeight() - height) * vValue;
+
     }
 
 }
