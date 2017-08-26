7e0ac3745213de9154a184c0af9b1a29a9a5820c
Harry Guthrie <GTGuthers@gmail.com>
2017-08-26 12:59:18 +1000

    Removing of name from resources and compiler fix

--- src/classes/Resources.java
+++ src/classes/Resources.java
@@ -21,23 +21,18 @@ public class Resources {
 
     private double amount;
     private double changeRate;
-    private String name;
     private ResourceType type;
 
     public Resources (Node.LandType lt){
-        type = ResourceType.getRandom();
+        type = ResourceType.getRandom(lt);
         amount = 1000 * resourceModifier(type) * landModifier(lt);
         changeRate = Math.random() * landModifier(lt);
-        name = resource_name(lt);
         }
 
     public void tick() {
         amount +=  (100 * changeRate);
     }
 
-    public String getName() {
-        return this.name;
-    }
 
     public double getAmount() {
         return this.amount;
@@ -140,220 +135,5 @@ public class Resources {
         }
         return modifier;
     }
-
-    private String resource_name(Node.LandType lt) {
-        String name = "";
-        Random rand = new Random();
-        int index = rand.nextInt(3);
-        switch (lt) {
-            case LAND:
-                switch (this.type) {
-                    case FOOD:
-                        switch (index) {
-                            case 0:
-                                name = "Berries";
-                                break;
-                            case 1:
-                                name = "Apples";
-                                break;
-                            case 2:
-                                name = "Rabbits";
-                                break;
-                        }
-                        break;
-                    case MINERAL:
-                        switch (index) {
-                            case 0:
-                                name = "Salt";
-                                break;
-                            case 1:
-                                name = "Rocks";
-                                break;
-                            case 2:
-                                name = "Copper";
-                                break;
-                        }
-                        break;
-                    case UTILITY:
-                        switch (index) {
-                            case 0:
-                                name = "Wood";
-                                break;
-                            case 1:
-                                name = "Rope";
-                                break;
-                            case 2:
-                                name = "Artistic Views";
-                                break;
-                        }
-                        break;
-                }
-                break;
-            case WATER:
-                switch (this.type) {
-                    case FOOD:
-                        switch (index) {
-                            case 0:
-                                name = "Tuna";
-                                break;
-                            case 1:
-                                name = "Salmon";
-                                break;
-                            case 2:
-                                name = "Sea Weed";
-                                break;
-                        }
-                        break;
-                    case MINERAL:
-                        switch (index) {
-                            case 0:
-                                name = "Salt";
-                                break;
-                            case 1:
-                                name = "Pearls";
-                                break;
-                            case 2:
-                                name = "Oil";
-                                break;
-                        }
-                        break;
-                    case UTILITY:
-                        name = "SHOULD NOT HAPPEN";
-                        break;
-                }
-                break;
-            case FOREST:
-                switch (this.type) {
-                    case FOOD:
-                        switch (index) {
-                            case 0:
-                                name = "Squirrel";
-                                break;
-                            case 1:
-                                name = "Nuts";
-                                break;
-                            case 2:
-                                name = "Fruits";
-                                break;
-                        }
-                        break;
-                    case MINERAL:
-                        switch (index) {
-                            case 0:
-                                name = "Gold";
-                                break;
-                            case 1:
-                                name = "Oil";
-                                break;
-                            case 2:
-                                name = "Steel";
-                                break;
-                        }
-                        break;
-                    case UTILITY:
-                        switch (index) {
-                            case 0:
-                                name = "Sap";
-                                break;
-                            case 1:
-                                name = "Vines";
-                                break;
-                            case 2:
-                                name = "Wood";
-                                break;
-                        }
-                        break;
-                }
-                break;
-            case MOUNTAIN:
-                switch (this.type) {
-                    case FOOD:
-                        switch (index) {
-                            case 0:
-                                name = "Mountain Goat";
-                                break;
-                            case 1:
-                                name = "";
-                                break;
-                            case 2:
-                                name = "";
-                                break;
-                        }
-                        break;
-                    case MINERAL:
-                        switch (index) {
-                            case 0:
-                                name = "";
-                                break;
-                            case 1:
-                                name = "";
-                                break;
-                            case 2:
-                                name = "";
-                                break;
-                        }
-                        break;
-                    case UTILITY:
-                        switch (index) {
-                            case 0:
-                                name = "";
-                                break;
-                            case 1:
-                                name = "";
-                                break;
-                            case 2:
-                                name = "";
-                                break;
-                        }
-                        break;
-                }
-                break;
-            case COASTAL:
-                switch (this.type) {
-                    case FOOD:
-                        switch (index) {
-                            case 0:
-                                name = "";
-                                break;
-                            case 1:
-                                name = "";
-                                break;
-                            case 2:
-                                name = "";
-                                break;
-                        }
-                        break;
-                    case MINERAL:
-                        switch (index) {
-                            case 0:
-                                name = "";
-                                break;
-                            case 1:
-                                name = "";
-                                break;
-                            case 2:
-                                name = "";
-                                break;
-                        }
-                        break;
-                    case UTILITY:
-                        switch (index) {
-                            case 0:
-                                name = "";
-                                break;
-                            case 1:
-                                name = "";
-                                break;
-                            case 2:
-                                name = "";
-                                break;
-                        }
-                        break;
-                }
-                break;
-        }
-        return name;
-    }
-
 
     }
